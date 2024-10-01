f8bbac7b2dc442c6324f85ac6d1b0be3b99682c7
pawarp19 <pranav19803@gmail.com>
2024-10-01 11:38:14 +0530

    new commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js'; 
-import './App.css'; 
+} from 'chart.js';
+import './App.css';
 import Appbar from './Appbar';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -30,7 +30,7 @@ ChartJS.register(
 );
 
 const App = () => {
-  const [month, setMonth] = useState('November'); 
+  const [month, setMonth] = useState('November');
   const [transactions, setTransactions] = useState([]);
   const [statistics, setStatistics] = useState({
     totalSales: 0,
@@ -39,7 +39,9 @@ const App = () => {
   });
   const [barChartData, setBarChartData] = useState([]);
   const [pieChartData, setPieChartData] = useState([]);
-  const [searchInput, setSearchInput] = useState(''); 
+  const [searchInput, setSearchInput] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     const fetchTransactions = async () => {
@@ -106,55 +108,51 @@ const App = () => {
 
   // Render statistics as cards
   const renderStatistics = () => {
-    const totalSalesPercentage = (statistics.totalSales / 10000) * 100; 
-  const soldItemsPercentage = (statistics.totalSold / 100) * 100; 
-  const notSoldItemsPercentage = (statistics.totalNotSold / 100) * 100; 
+    const totalSalesPercentage = (statistics.totalSales / 10000) * 100;
+    const soldItemsPercentage = (statistics.totalSold / 100) * 100;
+    const notSoldItemsPercentage = (statistics.totalNotSold / 100) * 100;
 
-  return (
-    <div className="stat-container">
-    <div className="stat-card">
-      <CircularProgressbar 
-        value={totalSalesPercentage} 
-        text={`$${statistics.totalSales.toFixed(2)}`} 
-        styles={{
-          path: { stroke: '#4caf50' }, // Path color
-          text: { fill: '#fff', fontSize: '15px',textShadow:'0 0 5px #4caf50, 0 0 10px #4caf50, 0 0 15px #4caf50' }, // Smaller text size
-          trail: { stroke: '#d6d6d6' }, // Trail color
-        }}
-      />
-    <h3  style={{color:'#4caf50'}}>Total Sales</h3>
-    </div>
-    <div className="stat-card">
-      <CircularProgressbar 
-        value={soldItemsPercentage} 
-        text={`${statistics.totalSold}`} 
-        styles={{
-          path: { stroke: '#2196F3' }, 
-          text: { fill: '#fff', fontSize: '15px',textShadow: '0 0 5px #2196F3, 0 0 10px #2196F3, 0 0 15px #2196F3' }, // Smaller text size
-          trail: { stroke: '#d6d6d6' },
-        }}
-      />
-      <h3 style={{color:'#2196F3'}}>Sold Items</h3>
-    </div>
-    <div className="stat-card">
-      <CircularProgressbar 
-        value={notSoldItemsPercentage} 
-        text={`${statistics.totalNotSold}`} 
-        styles={{
-          path: { stroke: '#f44336' }, 
-          text: { fill: '#fff', fontSize: '15px',textShadow: '0 0 5px #f44336, 0 0 10px #f44336, 0 0 15px #f44336' }, // Smaller text size
-          trail: { stroke: '#d6d6d6' }, 
-        }}
-      />
-      <h3  style={{color:'#f44336'}}>Not Sold Items</h3>
-    </div>
-  </div>
+    return (
+      <div className="stat-container">
+        <div className="stat-card">
+          <CircularProgressbar
+            value={totalSalesPercentage}
+            text={`$${statistics.totalSales.toFixed(2)}`}
+            styles={{
+              path: { stroke: '#4caf50' },
+              text: { fill: '#fff', fontSize: '15px', textShadow: '0 0 5px #4caf50, 0 0 10px #4caf50, 0 0 15px #4caf50' },
+              trail: { stroke: '#d6d6d6' },
+            }}
+          />
+          <h3 style={{ color: '#4caf50' }}>Total Sales</h3>
+        </div>
+        <div className="stat-card">
+          <CircularProgressbar
+            value={soldItemsPercentage}
+            text={`${statistics.totalSold}`}
+            styles={{
+              path: { stroke: '#2196F3' },
+              text: { fill: '#fff', fontSize: '15px', textShadow: '0 0 5px #2196F3, 0 0 10px #2196F3, 0 0 15px #2196F3' },
+              trail: { stroke: '#d6d6d6' },
+            }}
+          />
+          <h3 style={{ color: '#2196F3' }}>Sold Items</h3>
+        </div>
+        <div className="stat-card">
+          <CircularProgressbar
+            value={notSoldItemsPercentage}
+            text={`${statistics.totalNotSold}`}
+            styles={{
+              path: { stroke: '#f44336' },
+              text: { fill: '#fff', fontSize: '15px', textShadow: '0 0 5px #f44336, 0 0 10px #f44336, 0 0 15px #f44336' },
+              trail: { stroke: '#d6d6d6' },
+            }}
+          />
+          <h3 style={{ color: '#f44336' }}>Not Sold Items</h3>
+        </div>
+      </div>
     );
-  }
-
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5; 
+  };
 
   // Render transactions table with pagination
   const renderTable = () => {
@@ -168,12 +166,12 @@ const App = () => {
         (transaction.sold ? 'sold'.includes(lowerCaseSearch) : 'not sold'.includes(lowerCaseSearch))
       );
     });
-  
+
     // Calculate the indices for items to be displayed on the current page
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItems = filteredTransactions.slice(indexOfFirstItem, indexOfLastItem);
-  
+
     return (
       <div className="table-responsive">
         <table>
@@ -201,7 +199,7 @@ const App = () => {
                 <td>{transaction.title}</td>
                 <td>{transaction.description}</td>
                 <td>${transaction.price.toFixed(2)}</td>
-                <td>{transaction.dateOfSale || 'N/A'}</td> 
+                <td>{transaction.dateOfSale}</td>
                 <td>{transaction.category}</td>
                 <td>{transaction.sold ? 'Sold' : 'Not Sold'}</td>
               </tr>
@@ -211,143 +209,138 @@ const App = () => {
       </div>
     );
   };
-  
 
   // Render pagination controls
   const renderPagination = (totalItems) => {
     const totalPages = Math.ceil(totalItems / itemsPerPage);
 
     return (
-        <div className="pagination">
-            <button
-                onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-                disabled={currentPage === 1}
-                className="pagination-button"
-            >
-                Previous
-            </button>
-            <span className="page-info">Page {currentPage} of {totalPages}</span>
-            <button
-                onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
-                disabled={currentPage === totalPages}
-                className="pagination-button"
-            >
-                Next
-            </button>
-        </div>
+      <div className="pagination">
+        <button
+          onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+          disabled={currentPage === 1}
+          className="pagination-button"
+        >
+          Previous
+        </button>
+        <span className="page-info">Page {currentPage} of {totalPages}</span>
+        <button
+          onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+          disabled={currentPage === totalPages}
+          className="pagination-button"
+        >
+          Next
+        </button>
+      </div>
     );
   };
 
-
-
   return (
     <div>
-      < Appbar />
-    <div className="container">
-      
-
-      {/* Month selection container */}
-      <div className="filter-container">
-        <label htmlFor="month-dropdown" className="month-label">Select Month:</label>
-        <select
-          id="month-dropdown"
-          value={month}
-          onChange={handleMonthChange}
-          className="month-dropdown"
-        >
-          {['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'].map(m => (
-            <option key={m} value={m}>{m}</option>
-          ))}
-        </select>
-      </div>
-
-      {renderStatistics()} {/* Render the statistics cards */}
-
-      <h2 style={{textAlign:'center'}}>Transactions</h2>
-      
-      {/* Search Input */}
-      <div className="search-container">
-        <div className="search-wrapper">
-          <FontAwesomeIcon icon={faSearch} className="search-icon" color='secondary'/>
-          <input
-            type="text"
-            placeholder="Search transactions"
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)} 
-            className="search-input"
-          />
+      <Appbar />
+      <div className="container">
+        {/* Month selection container */}
+        <div className="filter-container">
+          <label htmlFor="month-dropdown" className="month-label">Select Month:</label>
+          <select
+            id="month-dropdown"
+            value={month}
+            onChange={handleMonthChange}
+            className="month-dropdown"
+          >
+            {['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'].map(m => (
+              <option key={m} value={m}>{m}</option>
+            ))}
+          </select>
         </div>
-      </div>
 
-      {renderTable()}
+        {renderStatistics()}
 
-      <div className="chart-container">
-        <div className="chart">
-          <h2 >Bar Chart</h2>
-          <Bar
+        <h2 style={{ textAlign: 'center' }}>Transactions</h2>
+
+        {/* Search Input */}
+        <div className="search-container">
+          <div className="search-wrapper">
+            <FontAwesomeIcon icon={faSearch} className="search-icon" color="secondary" />
+            <input
+              type="text"
+              placeholder="Search transactions"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              className="search-input"
+            />
+          </div>
+        </div>
+
+        {renderTable()}
+
+        {/* Render the pagination here */}
+        {renderPagination(transactions.length)}
+
+        <div className="chart-container">
+          <div className="chart">
+            <h2>Bar Chart</h2>
+            <Bar
               data={{
-                  labels: barChartData.map(d => d.range),
-                  datasets: [{
-                      label: '# of Items',
-                      data: barChartData.map(d => d.count),
-                      backgroundColor: 'rgba(75, 192, 192, 0.9)',
-                  }]
+                labels: barChartData.map(d => d.range),
+                datasets: [{
+                  label: '# of Items',
+                  data: barChartData.map(d => d.count),
+                  backgroundColor: 'rgba(75, 192, 192, 0.9)',
+                }]
               }}
               options={{
-                  responsive: true,
-                  maintainAspectRatio: false,
-                  scales: {
-                    x: {
-                      ticks: {
-                        color: 'white', 
-                      },
-                      
-                      grid: {
-                        color: 'rgba(255, 255, 255, 0.1)',
-                      },
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                  x: {
+                    ticks: {
+                      color: 'white',
                     },
-                    y: {
-                      ticks: {
-                        color: 'white', 
-                      },
-                      grid: {
-                        color: 'rgba(255, 255, 255, 0.1)', 
-                      },
-                    }
+                    grid: {
+                      color: 'rgba(255, 255, 255, 0.1)',
+                    },
                   },
-                  plugins: {
-                      legend: { position: 'top' },
-                      title: { display: true, text: 'Price Range Distribution',color:'white' },
-                  },
+                  y: {
+                    ticks: {
+                      color: 'white',
+                    },
+                    grid: {
+                      color: 'rgba(255, 255, 255, 0.1)',
+                    },
+                  }
+                },
+                plugins: {
+                  legend: { position: 'top' },
+                  title: { display: true, text: 'Price Range Distribution', color: 'white' },
+                },
               }}
-          />
-        </div>
-        <div className="chart">
-          <h2  >Pie Chart</h2>
-          <Pie
+            />
+          </div>
+          <div className="chart">
+            <h2>Pie Chart</h2>
+            <Pie
               data={{
-                  labels: pieChartData.map(d => d._id),
-                  datasets: [{
-                      label: '# of Items',
-                      data: pieChartData.map(d => d.count),
-                      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
-                  }]
+                labels: pieChartData.map(d => d._id),
+                datasets: [{
+                  label: '# of Items',
+                  data: pieChartData.map(d => d.count),
+                  backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
+                }]
               }}
               options={{
-                  responsive: true,
-                  maintainAspectRatio: false,
-                  plugins: {
-                      legend: { position: 'right',labels: {
-                    color: 'white', 
-                  } },
-                      title: { display: true, text: 'Category Distribution',color:'white' },
-                  },
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                  legend: { position: 'right', labels: { color: 'white' } },
+                  title: { display: true, text: 'Category Distribution', color: 'white' },
+                },
               }}
-          />
+            />
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
